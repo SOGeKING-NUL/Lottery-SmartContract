e76c963256bb648ef1bad139ac18ad11a1681e19
sogeking-nul <utsav.jana.5@gmail.com>
2025-05-06 21:09:25 +0530

    fixed import

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -6,7 +6,7 @@ import {Raffle} from "src/Raffle.sol";
 import {Vm} from "lib/forge-std/src/Vm.sol";
 import {DeployRaffle} from "script/DeployRaffle.s.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
-import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+import {VRFCoordinatorV2_5Mock} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 contract RaffleTest is Test{
 
